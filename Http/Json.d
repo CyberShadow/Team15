46f3c62aa52df5ce22ec861d52d3624777223569
Vladimir Panteleev <vladimir@thecybershadow.net>
2011-05-24 02:30:39 +0300

    Add some JSON parsing

--- Http/Json.d
+++ Http/Json.d
@@ -104,3 +104,214 @@ unittest
 	int[] arr = [1,5,7];
 	assert(toJson(arr) == `[1,5,7]`);
 }
+
+// -------------------------------------------------------------------------------------------
+
+import std.ctype;
+import std.utf;
+import std.conv;
+import Team15.Utils;
+
+private struct JsonParser
+{
+	string s;
+	int p;
+
+	char next()
+	{
+		enforce(p < s.length);
+		return s[p++];
+	}
+
+	string readN(uint n)
+	{
+		string r;
+		for (int i=0; i<n; i++)
+			r ~= next();
+		return r;
+	}
+
+	char peek()
+	{
+		enforce(p < s.length);
+		return s[p];
+	}
+
+	void skipWhitespace()
+	{
+		while (iswhite(peek))
+			p++;
+	}
+
+	void expect(char c)
+	{
+		enforce(next==c, c ~ " expected");
+	}
+
+	T read(T)()
+	{
+		static if (is(T==string))
+			return readString();
+		else
+		static if (is(T==bool))
+			return readBool();
+		else
+		static if (is(T : long))
+			return readInt!(T)();
+		else
+		static if (is(T U : U[]))
+			return readArray!(U)();
+		else
+		static if (is(T==struct))
+			return readObject!(T)();
+		else
+		static if (is(typeof(T.keys)) && is(typeof(T.values)) && is(typeof(T.keys[0])==string))
+			return readAA!(T)();
+		else
+		static if (is(T U : U*))
+			return readPointer!(U)();
+		else
+			static assert(0, "Can't decode " ~ T.stringof ~ " from JSON");
+	}
+
+	string readString()
+	{
+		skipWhitespace();
+		expect('"');
+		string result;
+		while (true)
+		{
+			auto c = next;
+			if (c=='"')
+				break;
+			else
+			if (c=='\\')
+				switch (next)
+				{
+					case '"':  result ~= '"'; break;
+					case '/':  result ~= '/'; break;
+					case '\\': result ~= '\\'; break;
+					case 'b':  result ~= '\b'; break;
+					case 'f':  result ~= '\f'; break;
+					case 'n':  result ~= '\n'; break;
+					case 'r':  result ~= '\r'; break;
+					case 't':  result ~= '\t'; break;
+					case 'u':  result ~= toUTF8([cast(wchar)fromHex(readN(4))]); break;
+					default: enforce(false, "Unknown escape");
+				}
+			else
+				result ~= c;
+		}
+		return result;
+	}
+
+	bool readBool()
+	{
+		skipWhitespace();
+		if (peek=='t')
+		{
+			enforce(readN(4) == "true", "Bad boolean");
+			return true;
+		}
+		else
+		{
+			enforce(readN(5) == "false", "Bad boolean");
+			return false;
+		}
+	}
+
+	T readInt(T)()
+	{
+		skipWhitespace();
+		T v;
+		string s;
+		char c;
+		while (c=peek, c=='-' || (c>='0' && c<='9'))
+			s ~= c, p++;
+		static if (is(T==byte))
+			return toByte(s);
+		else
+		static if (is(T==ubyte))
+			return toUbyte(s);
+		else
+		static if (is(T==short))
+			return toShort(s);
+		else
+		static if (is(T==ushort))
+			return toUshort(s);
+		else
+		static if (is(T==int))
+			return toInt(s);
+		else
+		static if (is(T==uint))
+			return toUint(s);
+		else
+		static if (is(T==long))
+			return toLong(s);
+		else
+		static if (is(T==ulong))
+			return toUlong(s);
+		else
+			static assert(0, "Don't know how to parse numerical type " ~ T.stringof);
+	}
+
+	T[] readArray(T)()
+	{
+		skipWhitespace();
+		expect('[');
+		skipWhitespace();
+		if (peek==']')
+			return [];
+		T[] result;
+		while(true)
+		{
+			result ~= read!(T)();
+			skipWhitespace();
+			if (peek==']')
+			{
+				p++;
+				return result;
+			}
+			else
+				expect(',');
+		}
+	}
+
+	T readObject(T)()
+	{
+		skipWhitespace();
+		expect('{');
+		skipWhitespace();
+		T v;
+		if (peek=='}')
+			return v;
+
+		while (true)
+		{
+			string jsonField = readString();
+			skipWhitespace();
+			expect(':');
+
+			bool found;
+			foreach (i, field; v.tupleof)
+				if (v.tupleof[i].stringof[2..$] == jsonField)
+				{
+					v.tupleof[i] = read!(typeof(v.tupleof[i]))();
+					found = true;
+					break;
+				}
+			enforce(found, "Unknown field " ~ jsonField);
+
+			skipWhitespace();
+			if (peek=='}')
+			{
+				p++;
+				return v;
+			}
+			else
+				expect(',');
+		}
+	}
+}
+
+T jsonParse(T)(string s) { return JsonParser(s).read!(T); }
